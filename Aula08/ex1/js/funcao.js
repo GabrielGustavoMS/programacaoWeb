af50766fac4035d5af152fbf3dba1eed5042895f
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-06-29 13:35:27 -0300

    Add files via upload

---
+++ Aula08/ex1/js/funcao.js
@@ -0,0 +1,11 @@
+function calculaValor(){
+    var tamanho = ($("input[name=tamanho]").val())
+    switch(tamanho){
+       case      
+    }
+}
+            
+            
+            idade =23
+            
+            
