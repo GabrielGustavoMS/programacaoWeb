20112d4692bf013cd732d9cbd02e44ffe69c4a64
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-06-29 13:36:38 -0300

    Add files via upload

---
+++ eventos/js/funcao.js
@@ -0,0 +1,36 @@
+ function calculaPreco(tamanho,ingredienteExtra){
+     var preço;
+     
+     switch(tamanho){
+         case "pequeno":
+             preço = 20;
+             break;
+         case "médio":
+             preço = 25;
+             break;
+         case "grande":
+             preço = 30;
+             break;
+     }
+     console.log(preço);
+     console.log(tamanho);
+     console.log(ingredienteExtra);
+     
+     if (ingredienteExtra == "Tomate" || ingredienteExtra =="Cebola"|| ingredienteExtra =="Azeitona"){
+         
+         preço = preço+7.5;
+         
+     }else{
+         preço = preço+11.5;
+     }
+     return preço;
+ }
+function verificaCor(preço){
+    if(preço<=30){
+        $("#pedido").addClass("azul");
+    }else if(preço>30 && preço<=40){
+        $("#pedido").addClass("vermelho");
+    }else{
+        $("#pedido").addClass("verde");
+    }
+}
